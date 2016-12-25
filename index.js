f1f29ba15b6d3dd7a116fe19e75ab3af0470a4d7
Umar Hansa <umar.hansa@gmail.com>
2016-12-25 22:44:30 +0000

    video db work

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mustacheExpress = require('mustache-express');
 const port = 3000;
 const express = require('express')
 const app = express()
+
 let Video;
 let VideoJob;
 
@@ -32,29 +33,72 @@ app.get('/details', (req, res) => {
 	return res.redirect(302, '/details/' + id);
 });
 
-app.get('/details/:id', (req, res) => {
+app.get('/details/:id', async (req, res) => {
 	const id = req.params.id;
 
 	if (!id) {
 		return res.redirect(302, '/');
 	}
 
-	res.render('details', {
+	let videoMatch = await Video.findOne({
+		where: {
+			video_id: id
+		}
+	});
+
+	const responseData = {
+		justCreated: false,
 		title: 'Video Details',
 		id
-	});
+	};
+
+	if (videoMatch) {
+		console.log('Video found!');
+		const job = await videoMatch.getVideoJob();
+
+		responseData.video = videoMatch.get();
+
+		if (job) {
+			responseData.job = job.get();
+		}
+	} else {
+		const video = await Video.create({
+			video_id: id,
+			video_provider: 'youtube',
+			video_duration: 200
+		});
+
+		const job = await video.createVideoJob({
+			status: 'queued'
+		});
+
+		responseData.video = video.get();
+		responseData.job = job.get();
+		responseData.justCreated = true;
+	}
+
+	responseData.jobsLength = await VideoJob.count();
+
+	console.log(responseData);
+	res.render('details', responseData);
 });
 
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('database', 'username', 'password', {
-	storage: __dirname + '/database.sqlite',
-	host: 'localhost',
-	dialect: 'sqlite'
-});
 
-const shouldDropTables = true;
+
+
+
+
+
 
 async function setupDB() {
+	const shouldDropTables = true;
+	const Sequelize = require('sequelize');
+	const sequelize = new Sequelize('database', 'username', 'password', {
+		storage: __dirname + '/database.sqlite',
+		host: 'localhost',
+		dialect: 'sqlite'
+	});
+
 	Video = sequelize.define('video', {
 		video_id: {
 			type: Sequelize.STRING,
@@ -65,39 +109,36 @@ async function setupDB() {
 		video_duration: Sequelize.INTEGER
 	});
 
-	VideoJob = sequelize.define('video_job', {
+	VideoJob = sequelize.define('videoJob', {
 		foo: Sequelize.STRING,
 		status: Sequelize.STRING
 	});
 
-	Video.belongsTo(VideoJob);
+	Video.hasOne(VideoJob);
+	VideoJob.belongsTo(Video);
 
-	sequelize.sync({
-		force: shouldDropTables ? true : false
+	return await sequelize.sync({
+		force: shouldDropTables ? true : false,
+		logging: false
 	});
-}
-
-async function init() {
-	setupDB();
 }
 
 async function dbTest() {
 	const video1 = await Video.create({
-		video_id: 'fdsgfdg',
+		video_id: 'zzzzz',
 		video_provider: 'youtube',
 		video_duration: 100
-	});
-
-	const videoJob1 = await VideoJob.create({
-		foo: 'hello, the relationship works!',
-		status: 'queued'
 	});
 
 	const video2 = await Video.create({
 		video_id: 'aaabbb',
 		video_provider: 'youtube',
-		video_duration: 200,
-		videoJobId: videoJob1.id
+		video_duration: 200
+	});
+
+	const videoJob1 = await video2.createVideoJob({
+		foo: 'wahoo',
+		status: 'queued',
 	});
 
 	const videoQuery = await Video.findOne({
@@ -106,27 +147,24 @@ async function dbTest() {
 		}
 	});
 
-	console.log(videoQuery.get());
-	console.log((await videoQuery.getVideo_job()).get());
-
 	const jobQuery = await VideoJob.findOne({
 		where: {
 			id: 1
 		}
 	});
-	console.log('\n\n');
-	console.log((await getVideoFromJob(Video, jobQuery.id)).get());
+
+	// console.log(videoQuery.get());
+	// console.log((await videoQuery.getVideoJob()).get());
+	// console.log('\n\n');
+	// console.log((await jobQuery.getVideo()).get());
+	// console.log(video1.constructor.prototype);
 }
 
-async function getVideoFromJob(VideoModel, jobId) {
-	return await VideoModel.findOne({
-		where: {
-			videoJobId: jobId
-		}
-	});
+async function init() {
+	await setupDB();
+	// await dbTest();
 }
 
-// dbTest();
-
+init();
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
